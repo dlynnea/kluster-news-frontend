b9bf7249f626057bbd5326a47257fa186975be22
Dani Matton <danielle@Danis-MacBook-Pro.local>
2020-03-03 17:39:41 -0700

    styling for unsplash images

--- src/containers/Unsplash.js
+++ src/containers/Unsplash.js
@@ -1,48 +1,77 @@
 import React, {Component} from 'react'
-import ImageList from './ImageList'
-import SearchForm from './SearchForm'
-// import axios from 'axios'
+import Pagination from './Pagination'
+import List from './List'
+import axios from 'axios'
 
 const access = 'wMJUZTzA_Z3xLfQySAqtXXk42gnNYpHEqL4qUpzOoz8';
 
+const LOAD_STATE = {
+  SUCCESS: 'SUCCESS',
+  ERROR: 'ERROR',
+  LOADING: 'LOADING'
+};
+
 class Unsplash extends Component {
-
-    state = {
-        imgs: {
-            results: []
-        },
-        loadingState: true
+  constructor() {
+    super();
+    this.state = {
+      photos: {
+          results: []
+      },
+      totalPhotos: 0,
+      perPage: 10,
+      currentPage: 1,
+      loadState: LOAD_STATE.LOADING
     }
-
-    componentDidMount() {
-        this.fetchUnsplash();
-    }
-
-    fetchUnsplash = () => {
-        fetch(`https://api.unsplash.com/search/photos/?page=1&per_page=10&query=botanical&client_id=${access}`)
-       .then(response => response.json())
-       .then(imgs => this.setState({ imgs }))
+  }
+  
+  componentDidMount() {
+    this.fetchPhotos(this.state.currentPage);
+  }
+  
+  fetchPhotos(page) {
+    var self = this;
+    const { perPage } = this.state;
+    const { appId, baseUrl } = this.props;
+    const options = {
+      params: {
+        client_id: appId,
+        page: page,
+        per_page: perPage
+      }
     };
-
-    render() { 
-
-        return (
-            <div>
-                <div className="main-header">
-                    <div className="inner">
-                        {/* <ImageList imgs={this.state.imgs} /> */}
-                        {/* <h1 className="main-title">Search..</h1> */}
-                        <SearchForm onSearch={this.fetchUnsplash} />
-                    </div>
-                </div>
-                <div className="main-content">
-                    {/* {this.state.loadingState
-                        ? <p>Loading..</p> */}
-                        <ImageList imgs={this.state.imgs} />
-                </div>
-            </div>
-        );
-    }
+    
+    this.setState({ loadState: LOAD_STATE.LOADING });
+    axios.get(`https://api.unsplash.com/search/photos/?page=1&per_page=10&query=botanical&client_id=${access}`, options)
+      .then((response) => {
+        self.setState({
+          photos: response.data,
+          totalPhotos: parseInt(response.headers['x-total']),
+          currentPage: page,
+          loadState: LOAD_STATE.SUCCESS
+        });
+      })
+      .catch(() => {
+        this.setState({ loadState: LOAD_STATE.ERROR });
+      });
+  }
+  
+  render() {
+    return (
+      <div className="app">
+        <Pagination
+          current={this.state.currentPage}
+          total={this.state.totalPhotos} 
+          perPage={this.state.perPage} 
+          onPageChanged={this.fetchPhotos.bind(this)}
+        />
+        {this.state.loadState === LOAD_STATE.LOADING
+            ? <div className="loader"></div>
+            : <List photos={this.state.photos} />  
+          }
+      </div>
+    )
+  }
 }
- 
+
 export default Unsplash;
