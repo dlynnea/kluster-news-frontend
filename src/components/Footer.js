4327b1c367ba07086f1a596b2df6f35ef48c9de4
Dani Matton <danielle@Danis-MacBook-Pro.local>
2020-03-09 11:07:10 -0600

    fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => <footer className='footer text-center'>
-        <p className='footer'><strong>fuck off</strong> &hearts; | &copy; {new Date().getFullYear()}</p>
+        <p className='footer'> &hearts; | &copy; {new Date().getFullYear()}</p>
 </footer>
 
 export default Footer;
