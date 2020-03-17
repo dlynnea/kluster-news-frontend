5a0fa11dd8a3fb9ca26c120cf7fe4ef5fb6c7cea
Dani Matton <danielle@Danis-MacBook-Pro.local>
2020-03-17 13:53:14 -0600

    minor styling changes

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 const About = () => (
   <div>
-    <h3><strong>Welcome to kurator,</strong> a content aggregator with all of your favorite sources!</h3><br/>
+    <h3><strong>Welcome to Curator,</strong> a content aggregator for your daily media desires</h3><br/>
     <p> Select <strong>Your News</strong> to filter through article titles.</p>
-    <p> Select <strong>Top Headlines</strong> to select a specific source of news.</p>
-    <p> I've sourced __ news using web crawlers I've wrote in Pythono using Scrapy</p>
-    <p> I've sourced everything in Your News using a News API provided by NewsApi.org</p>
+    <p> Select <strong>Mainstream</strong> to select a specific source of news.</p>
+    <p> I've sourced everything in Your News/Mainstream using a News API provided by NewsApi.org</p>
+    <p> Using the Unsplash API, I've queried botanical results for some fun inspiration.</p>
+    <p> I've sourced science, tech, travel and wellness news using web crawlers written in Python using the Scrapy Framework</p><br></br><br></br>
+    <p> If you're experiencing issues with the website, please feel free to reach out to me via email, danimatton@gmail.com</p>
   </div>
 )
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Footer = () => <footer className='footer text-center'>
-        <p className='footer'> &hearts; | &copy; {new Date().getFullYear()}</p>
+const Footer = () => <footer className='footer'>
+        <p className='footer'> created by Danielle Matton &copy; {new Date().getFullYear()}</p>
 </footer>
 
 export default Footer;
