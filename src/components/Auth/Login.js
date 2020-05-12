7dba54e92ecabf134e562195c04b4083c1b43a9c
dlynnea <danimatton@gmail.com>
2020-05-11 21:25:09 -0700

    integrating new form features

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom'
 // import PropTypes from 'prop-types';
 
 class Login extends Component {
@@ -20,24 +21,59 @@ class Login extends Component {
 
     render() {
         return(
-            <form onSubmit={event => this.props.handleLogin(event, this.state, this.props.history)}>
-                <h4>LOG IN</h4>
-                <input
-                type="text"
-                name="username"
-                placeholder="username"
+        <section className="auth-container">
+        <h1 className="lg primary-txt">Login</h1>
+        <form className="form auth-form" onSubmit={event => this.props.handleLogin(event, this.state, this.props.history)}>
+            <div className="form-input form-element">
+                <input 
+                className="form-element-field" 
+                type="username" 
+                placeholder="Username"
                 value={this.state.username}
                 onChange={this.handleChange}
-                />
-                <input
-                type="password"
+                name="username"
+                required />
+                <div className="form-element-bar"></div>
+            <label className="form-element-label">Username</label>
+            </div>
+            <div className="form-input form-element">
+                <input 
+                className="form-element-field" 
+                type="password" 
+                placeholder="Password"
                 name="password"
-                placeholder="password"
                 value={this.state.password}
                 onChange={this.handleChange}
-                />
-                <input className="input-btn" type="submit" />
-            </form>
+                required
+                minLength="6" />
+                <div className="form-element-bar"></div>
+            <label className="form-element-label">Password</label>
+            </div>
+            <input type="submit" className="btn btn-primary" value="Login" />
+        <p className="my-1 primary-txt">Don't have an account? <Link to='/register'>Sign Up</Link></p>
+        </form>
+        </section>
+
+
+
+            // <form onSubmit={event => this.props.handleLogin(event, this.state, this.props.history)}>
+            //     <h4>LOG IN</h4>
+            //     <input
+            //     type="text"
+            //     name="username"
+            //     placeholder="username"
+            //     value={this.state.username}
+            //     onChange={this.handleChange}
+            //     />
+            //     <input
+            //     type="password"
+            //     name="password"
+            //     placeholder="password"
+            //     value={this.state.password}
+            //     onChange={this.handleChange}
+            //     />
+            //     <input className="input-btn" type="submit" />
+            // </form>
         )
     }
 }
--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom'
 // import PropTypes from 'prop-types';
 
 class Register extends Component {
@@ -21,31 +22,79 @@ class Register extends Component {
 
     render() {
         return(
-            <form onSubmit={event => this.props.handleSignup(event, this.state, this.props.history)}>
-            <h4>SIGN UP</h4>
-            <input
-            type="text"
-            name="name"
-            placeholder="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-            />
-            <input
-            type="text"
-            name="username"
-            placeholder="username"
-            value={this.state.username}
-            onChange={this.handleChange}
-            />
-            <input
-            type="password"
-            name="password"
-            placeholder="password"
-            value={this.state.password}
-            onChange={this.handleChange}
-            />
-            <input className="input-btn" type="submit" />
+
+<section className="auth-container">
+        <h1 className="lg primary-txt">Create an Account</h1>
+        <form className="form auth-form" onSubmit={event => this.props.handleLogin(event, this.state, this.props.history)}>
+            <div className="form-input form-element">
+                <input 
+                className="form-element-field" 
+                type="Name" 
+                placeholder="Name"
+                value={this.state.name}
+                onChange={this.handleChange}
+                name="Name"
+                required />
+                <div className="form-element-bar"></div>
+            <label className="form-element-label">Name</label>
+            </div>
+            <div className="form-input form-element">
+                <input 
+                className="form-element-field" 
+                type="username" 
+                placeholder="Username"
+                value={this.state.username}
+                onChange={this.handleChange}
+                name="username"
+                required />
+                <div className="form-element-bar"></div>
+            <label className="form-element-label">Username</label>
+            </div>
+            <div className="form-input form-element">
+                <input 
+                className="form-element-field" 
+                type="password" 
+                placeholder="Password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+                required
+                minLength="6" />
+                <div className="form-element-bar"></div>
+            <label className="form-element-label">Password</label>
+            </div>
+            <input type="submit" className="btn btn-primary" value="Register" />
+        <p className="my-1 primary-txt">Already have an account? <Link to='/login'>Login</Link></p>
         </form>
+        </section>
+
+
+
+        //     <form onSubmit={event => this.props.handleSignup(event, this.state, this.props.history)}>
+        //     <h4>SIGN UP</h4>
+        //     <input
+        //     type="text"
+        //     name="name"
+        //     placeholder="name"
+        //     value={this.state.name}
+        //     onChange={this.handleChange}
+        //     />
+        //     <input
+        //     type="text"
+        //     name="username"
+        //     placeholder="username"
+        //     value={this.state.username}
+        //     onChange={this.handleChange}
+        //     />
+        //     <input
+        //     type="password"
+        //     name="password"
+        //     placeholder="password"
+        //     value={this.state.password}
+        //     onChange={this.handleChange}
+        //     />
+        //     <input className="input-btn" type="submit" />
+        // </form>
         )
     }
 }
